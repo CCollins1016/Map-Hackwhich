4f590f1e7b91e082db30e95183441600ee90427b
CCollins1016 <ccollins@bsd220.org>
2026-02-09 10:23:35 -0600

    Initial Commit

---
+++ Map Hackwhich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Map Hackwhich
+//
+//  Created by Christian Collins on 2/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Map Hackwhich/Map_HackwhichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwhichApp.swift
+//  Map Hackwhich
+//
+//  Created by Christian Collins on 2/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwhichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
